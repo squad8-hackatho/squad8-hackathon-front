30554bd0ac061cf92447bc03be73a95d62e09db7
Beatriz Paixão <biaperon2001@gmail.com>
2022-04-01 15:18:25 -0300

    Arquivo base

---
+++ back/technicalshare/src/main/java/com/fcamara/technicalshare/TechnicalshareApplication.java
@@ -0,0 +1,13 @@
+package com.fcamara.technicalshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalshareApplication.class, args);
+	}
+
+}
---
+++ back/technicalshare/src/test/java/com/fcamara/technicalshare/TechnicalshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.technicalshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
