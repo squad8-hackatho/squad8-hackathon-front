58d44f513ef977b12cb221767f64259df8a3fbd5
BrunoMundim <brunomundimfranco@gmail.com>
2022-04-02 21:47:43 -0300

    Primeiros componentes de uma página Perfil

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages';
+import Profile from '../pages/Profile/Profile';
 
 function OurRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="profile" element={<Profile />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
