2c3c8c6cd88f07b00b339b9c258539ba62dddce7
Carlos-Xavier <cexb@ic.ufal.br>
2022-04-09 22:08:29 -0300

    :sparkles: feat:  novos campos em cadastro e tirando modais.

---
+++ src/pages/system/Register.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Main, Button, Form, P, H2 } from './styles';
+import RegisterPartOne from './RegisterPartOne';
+import RegisterPartTwo from './RegisterPartTwo';
+
+function Register() {
+  const [name, setName] = useState('');
+  const [bio, setBio] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [next, setNext] = useState(false);
+  const [cargo, setCargo] = useState('');
+
+  const onSubmit = () => {
+    console.log(name, bio, email, password, cargo);
+  };
+
+  console.log(next);
+  return (
+    <Main>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          return onSubmit();
+        }}
+      >
+        <H2>
+          Technical<strong>Share</strong>
+        </H2>
+        {!next ? (
+          <RegisterPartOne
+            name={name}
+            setName={setName}
+            setBio={setBio}
+            setPassword={setPassword}
+            setEmail={setEmail}
+            password={password}
+            email={email}
+            setNext={setNext}
+          />
+        ) : (
+          <RegisterPartTwo setCargo={setCargo} />
+        )}
+
+        <P>
+          Já tem uma conta ?
+          <Link to="/login">
+            <Button type="button">Entrar</Button>
+          </Link>
+        </P>
+      </Form>
+    </Main>
+  );
+}
+
+export default Register;
